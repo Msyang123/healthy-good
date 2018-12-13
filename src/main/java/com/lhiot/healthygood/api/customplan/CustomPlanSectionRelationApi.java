b8c406d4361bfa83945b3f2d630868fb635937c2
hufan <282854419@qq.com>
2018-12-13 10:43:44 +0800

    添加定制板块新增和删除接口

--- src/main/java/com/lhiot/healthygood/api/customplan/CustomPlanSectionRelationApi.java
+++ src/main/java/com/lhiot/healthygood/api/customplan/CustomPlanSectionRelationApi.java
@@ -3,6 +3,7 @@ package com.lhiot.healthygood.api.customplan;
 import com.leon.microx.web.result.Tips;
 import com.leon.microx.web.session.Sessions;
 import com.leon.microx.web.swagger.ApiParamType;
+import com.lhiot.healthygood.domain.customplan.CustomPlanSectionRelation;
 import com.lhiot.healthygood.service.customplan.CustomPlanSectionRelationService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
@@ -11,9 +12,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author hufan created in 2018/11/27 14:42
@@ -43,5 +42,31 @@ public class CustomPlanSectionRelationApi {
 
         Tips tips = customPlanSectionRelationService.updateRelationList(sectionId, planIds, sorts);
         return tips.err() ? ResponseEntity.badRequest().body("批量修改定制版块与定制计划关系失败！") : ResponseEntity.ok().build();
+    }
+
+    @Sessions.Uncheck
+    @ApiOperation("添加定制版块与定制计划关系(后台)")
+    @ApiImplicitParam(paramType = ApiParamType.BODY, name = "customPlanSectionRelation", value = "定制版块与定制计划关系信息", dataType = "CustomPlanSectionRelation", required = true)
+    @PostMapping("/custom-plan-section-relations")
+    public ResponseEntity create(@RequestBody CustomPlanSectionRelation customPlanSectionRelation) {
+        log.debug("批量修改定制版块与定制计划关系\t param:{}", customPlanSectionRelation);
+
+        Tips tips = customPlanSectionRelationService.addRelation(customPlanSectionRelation);
+        if (tips.err()) {
+            return ResponseEntity.badRequest().body(tips.getMessage());
+        }
+        Long relationId = Long.valueOf(tips.getMessage());
+        return relationId > 0 ? ResponseEntity.ok().build() : ResponseEntity.badRequest().body("添加商品与版块关系记录失败！");
+    }
+
+
+    @Sessions.Uncheck
+    @ApiOperation("根据关联Id删除定制版块与定制计划架关系(后台)")
+    @ApiImplicitParam(paramType = ApiParamType.PATH, name = "id", value = "关系Id", dataType = "Long", required = true)
+    @DeleteMapping("/custom-plan-section-relations/{id}")
+    public ResponseEntity delete(@PathVariable("id") Long relationId) {
+        log.debug("根据关联Id删除定制版块与定制计划架关系\t relationId: {}", relationId);
+
+        return customPlanSectionRelationService.deleteRelation(relationId.toString()) ? ResponseEntity.noContent().build() : ResponseEntity.badRequest().body("根据关联Id删除定制版块与定制计划架关系失败！");
     }
 }
