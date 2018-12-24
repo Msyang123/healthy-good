a4babc97e99516f4212bff0521dc25624c78653f
yangjiawen <348235142@qq.com>
2018-12-24 14:09:20 +0800

    session权限添加

--- src/main/java/com/lhiot/healthygood/feign/ImsServiceFeign.java
+++ src/main/java/com/lhiot/healthygood/feign/ImsServiceFeign.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
 
 @Component
-@FeignClient("IMS-SERVICE-V1-0-YANGJIAWEN")
+@FeignClient("IMS-SERVICE-V1-0")
 public interface ImsServiceFeign {
     //商城用户访问权限session url集
     @RequestMapping(value = "/ims-operation/mall-user", method = RequestMethod.GET)
